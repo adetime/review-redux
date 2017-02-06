b69184c93db77ff6656190c948d8fc4bd5de9a32
Adeildo Amorim <adeildoamorim@gmail.com>
2017-02-06 10:03:27 -0800

    basic redux counter

---
+++ src/reducers/counterApp.js
@@ -0,0 +1,23 @@
+function counter(state, action) {
+  if ( typeof state === 'undefined' ) {
+    return 0;
+  }
+  if (action.type === "INCREMENT") {
+    return state + 1;
+  } else if (action.type==="DECREMENT") {
+    return state - 1;
+  } else {
+    return state;
+  }
+
+  switch (expression) {
+    case expression:
+
+      break;
+    default:
+
+  }
+
+}
+
+export { counter };
